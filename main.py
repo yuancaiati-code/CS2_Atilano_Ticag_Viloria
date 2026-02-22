a937349e9a54f2ac6a86c3244b0727c7ae2588b1
Yuan cai L. Atilano <ylatilano@carc.pshs.edu.ph>
2026-02-22 11:07:11 +0800

    Hi uwu I got the base of the program with save files -02/22/2026 save1,2,3 are just place holders for now

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 import json
 import time
 
+file = ""
 
+save =int(input("which save file do you want to load 1 , 2 , or 3: "))
+
+if save == 1:
+    file = "save1.json"
+elif save == 2:
+    file = "save2.json"
+elif save == 3:
+    file = "save3.json"
+
+time.sleep(0.2)
+print()
+print("loading file...")
+time.sleep(2)
+print()
+print()
 
 try:
-    filename = "save1.json"
+    filename = file
     with open(filename, 'r') as file:
         data = json.load(file)
 
 
 
     print("| Main Menu |")
-    print("")
+    print("1. john pork")
+    print("2. 67")
+    print("3. mustard")
+    print("4. Tutorial")
+    print()
+    option = int(input("What would you want to do today? : "))
 
+    if option == 1:
+        print("do something idk")
+
+    elif option == 2:
+        print("what does this do?")
+
+    elif option == 3:
+        print("Idk at this point")
+
+    elif option == 4:
+        print("Just mess around")
+        print("Idk what this program does")
 
 
 
