744237e574822abf6e1195a40a6694c66d0cb13b
Student <jabuasen@carc.pshs.edu.ph>
2026-03-18 09:34:42 +0800

    Changing file names

--- ChronoSmart.py
+++ ChronoSmart.py
@@ -34,42 +34,43 @@ try:
     def show_sched():
         while True:
             user_input = int(input("\nWhat schedule would you like to see?\n 1. Monday\n 2. Tuesday \n 3. Wednesday \n 4. Thursday \n 5. Friday \n 0. Menu\nEnter(1,2,3,4,5,0): "))
-            print("Schedule:")
-            print("Time -------> Period --------> Teacher")
-            if user_input == 1:
-                monday_schedule = []
-                for classes in mon:
-                    monday_schedule.append((classes["time"], classes["period"], classes["teacher"]))
-                for clas, period, teacher in monday_schedule:
-                    print(f"{clas} : {period} : {teacher}")
+            if user_input in list[1,2,3,4,5]:
+                print("Schedule:")
+                print("Time -------> Period --------> Teacher")
+                if user_input == 1:
+                    monday_schedule = []
+                    for classes in mon:
+                        monday_schedule.append((classes["time"], classes["period"], classes["teacher"]))
+                    for clas, period, teacher in monday_schedule:
+                        print(f"{clas} : {period} : {teacher}")
 
-            elif user_input == 2:
-                tuesday_schedule = []
-                for classes in tue:
-                    tuesday_schedule.append((classes["time"], classes["period"], classes["teacher"]))
-                for clas, period, teacher in tuesday_schedule:
-                    print(f"{clas} : {period} : {teacher}")
+                elif user_input == 2:
+                    tuesday_schedule = []
+                    for classes in tue:
+                        tuesday_schedule.append((classes["time"], classes["period"], classes["teacher"]))
+                    for clas, period, teacher in tuesday_schedule:
+                        print(f"{clas} : {period} : {teacher}")
 
-            elif user_input == 3:
-                wednesday_schedule = []
-                for classes in wed:
-                    wednesday_schedule.append((classes["time"], classes["period"], classes["teacher"]))
-                for clas, period, teacher in wednesday_schedule:
-                    print(f"{clas} : {period} : {teacher}")
+                elif user_input == 3:
+                    wednesday_schedule = []
+                    for classes in wed:
+                        wednesday_schedule.append((classes["time"], classes["period"], classes["teacher"]))
+                    for clas, period, teacher in wednesday_schedule:
+                        print(f"{clas} : {period} : {teacher}")
 
-            elif user_input == 4:
-                thursday_schedule = []
-                for class_adelfa in thu:
-                    thursday_schedule.append((class_adelfa["time"], class_adelfa["period"], class_adelfa["teacher"]))
-                for class_meet, period, teacher in thursday_schedule:
-                    print(f"{clas} : {period} : {teacher}")
+                elif user_input == 4:
+                    thursday_schedule = []
+                    for class_adelfa in thu:
+                        thursday_schedule.append((class_adelfa["time"], class_adelfa["period"], class_adelfa["teacher"]))
+                    for class_meet, period, teacher in thursday_schedule:
+                        print(f"{clas} : {period} : {teacher}")
 
-            elif user_input == 5:
-                friday_schedule = []
-                for classes in fri:
-                    friday_schedule.append((classes["time"], classes["period"], classes["teacher"]))
-                for clas, period, teacher in friday_schedule:
-                    print(f"{clas} : {period} : {teacher}")
+                elif user_input == 5:
+                    friday_schedule = []
+                    for classes in fri:
+                        friday_schedule.append((classes["time"], classes["period"], classes["teacher"]))
+                    for clas, period, teacher in friday_schedule:
+                        print(f"{clas} : {period} : {teacher}")
 
             #If the user types in 0, this asks the user if he or she would want to go back to the menu or not.
             elif user_input == 0  :
@@ -98,33 +99,34 @@ try:
     def update_classes():
         while True:
             day = int(input("What day do you want to update?\n 1. Monday\n 2. Tuesday\n 3. Wednesday\n 4. Thursday\n 5. Friday\n 0. Back to menu\nEnter (1,2,3,4,5,0): "))
-            class_update = input("What class will be updated(time)?")
-            update = input("What do you wanna change (period, or teacher)?")
-            change = input("What will you change it to?")
-            if day == 1:
-                for classes in mon:
-                    if class_update == classes["time"]:
-                        classes[update] = change
+            if day in list[1,2,3,4,5]:
+                class_update = input("What class will be updated(time)?")
+                update = input("What do you wanna change (period, or teacher)?")
+                change = input("What will you change it to?")
+                if day == 1:
+                    for classes in mon:
+                        if class_update == classes["time"]:
+                            classes[update] = change
 
-            elif day == 2:
-                for classes in tue:
-                    if class_update == classes["time"]:
-                        classes[update] = change
+                elif day == 2:
+                    for classes in tue:
+                        if class_update == classes["time"]:
+                            classes[update] = change
 
-            elif day == 3:
-                for classes in wed:
-                    if class_update == classes["time"]:
-                        classes[update] = change
+                elif day == 3:
+                    for classes in wed:
+                        if class_update == classes["time"]:
+                            classes[update] = change
 
-            elif day == 4:
-                for classes in thu:
-                    if class_update == classes["time"]:
-                        classes[update] = change
+                elif day == 4:
+                    for classes in thu:
+                        if class_update == classes["time"]:
+                            classes[update] = change
 
-            elif day == 5:
-                for classes in fri:
-                    if class_update == classes["time"]:
-                        classes[update] = change
+                elif day == 5:
+                    for classes in fri:
+                        if class_update == classes["time"]:
+                            classes[update] = change
 
             elif day == 0:
                 response = input("Would you like to go back to the menu (Y/N)? ")
@@ -139,13 +141,18 @@ try:
                     if response.upper() == "N":
                         break
 
-    #4.
+    #4.\
+    #    def update_tasks():
+    #    for task in tasks:
+    #        if task["progress"] == "Submitted":
+    #            del task[tasks.index(tasks)]
+    def update_tasks():
+       pass
 
 
     #This is for the user interface, which decides what is needed to access based on the user's decision.
     def menu():
         while True:
-            print("Welcome to ChronoSmart!\nI am Chron, here to help you manage your time\nJust enter the number for the corresponding choices. \nTake a look around, this is our menu:")
             ans = int(input("Menu: \n 1. Check Tasks\n 2. Check Schedule\n 3. Update Class\n 4. Update Tasks\n 0. Exit\nWhat do you want to do? (1,2,3,4, 0): "))
             if ans == 1:
                 check_tasks()
@@ -167,6 +174,14 @@ try:
             else:
                 print("Invalid input.")
 
+    print("Welcome to ChronoSmart!")
+    time.sleep(1)
+    print("I am Chron, here to help you manage your time")
+    time.sleep(1)
+    print("Just enter the number for the corresponding choices.")
+    time.sleep(1)
+    print("Take a look around, this is our menu:")
+    time.sleep(1)
     menu()
 except FileNotFoundError:
     print("Error: The file 'data.json' was not found")
